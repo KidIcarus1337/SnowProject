f29a83d5421de49fe45ec904e961a3b1dbf06581
KidIcarus1337 <kid_icarus3@yahoo.com>
2014-05-21 12:55:27 -0500

    Message with an exclamation point\!

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-    $("#review_radios > input").click(function() {
-        console.log("your mom");
-    });
+    /*function reviews_slideshow() {
+        $("#review_radios > input").click(function() {
+            
+        });
+        var cycling - setInterval(function() {
+            switch ($("[checked]").val()) {
+                case "first_review":
+                    $('[value="second_review"]').prop("checked", true);
+                    $('[value="second_review"]').fadeIn(0);
+                    $('[value="first_review"]').animate({left:"0"});
+                    
+            }
+        }, 5000);
+    });*/
     $(".social_link").tooltip();
+    var myDataRef = new Firebase("https://snow-project.firebaseio.com/“);
 });
