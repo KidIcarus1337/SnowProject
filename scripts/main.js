01235fe146fec0d4d417120a458d34669002f28b
KidIcarus1337 <kid_icarus3@yahoo.com>
2014-06-03 00:50:42 -0500

    Custom request handlers ftw\!

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,18 @@ $(document).ready(function() {
         errors.push(element_target);
     }
     
+    function disable_elements() {
+        $(".form_button").prop("disabled", true);
+        $(".pop_target").prop("disabled", true);
+        $(".checkbox_container").prop("disabled", true);
+    }
+    
+    function enable_elements() {
+        $(".form_button").prop("disabled", false);
+        $(".pop_target").prop("disabled", false);
+        $(".checkbox_container").prop("disabled", false);
+    }
+    
     $(".carousel").on("slide.bs.carousel", function() {
         $(".carousel").carousel('pause');
         setTimeout(function() {
@@ -35,13 +47,12 @@ $(document).ready(function() {
     });
     
     $(".social_link").tooltip();
-    var sp_db = new Firebase("https://snow-project.firebaseio.com/");
     
-    $(".log_section button").mousedown(function() {
+    $(".log_section button, #overlay_button").mousedown(function() {
         $(this).css({"background-color":"#CCCCCC", "border-color":"#00A1A1","color":"#00A1A1"});
     });
     
-    $(".log_section button").on("mouseup mouseleave", function() {
+    $(".log_section button, #overlay_button").on("mouseup mouseleave", function() {
         $(this).css({"background-color":"#FFF", "border-color":"#00E6E6","color":"#00E6E6"});
     });
     
@@ -53,7 +64,10 @@ $(document).ready(function() {
         $("#shoveler_container").popover("destroy");
     });
     
+    var sp_fb = new Firebase("https://snow-project.firebaseio.com/accounts/")
+    
     $("#sign_up_button").click(function() {
+        disable_elements();
         $(".pop_target").each(function() {
             $(this).popover("destroy");
         });
@@ -69,6 +83,8 @@ $(document).ready(function() {
         
         if (email === "") {
             pop_set("#email", ".popover:contains(email)", "Please provide an email address.", "left", errors);
+        } else if (email.indexOf("@") === -1) {
+            pop_set("#email", ".popover:contains(Invalid email)", "Invalid email address.", "left", errors);
         }
         if (user_name === "") {
             pop_set("#user_name", ".popover:contains(user)", "Please provide a user name.", "left", errors);
@@ -84,23 +100,46 @@ $(document).ready(function() {
         if (shoveler === false && poster === false) {
             pop_set("#shoveler_container", ".popover:contains(role)", "Please choose at least one role.", "left", errors);
         }
+        sp_fb.once("value", function(dataSnapshot) {
+            dataSnapshot.forEach(function(childSnapshot) {
+                if (email === childSnapshot.child("email").val()) {
+                    pop_set("#email", ".popover:contains(That email)", "That email address has already been registered.", "left", errors);
+                }
+                if (user_name === childSnapshot.child("user_name").val()) {
+                    pop_set("#user_name", ".popover:contains(That user name)", "That user name has already been taken.", "left", errors);
+                }
+            });
+        });
         setTimeout(function() {
             if (errors.length > 0) {
                 for (var each in errors) {
                     $(errors[each]).popover("show");
                 }
+                enable_elements();
+            } else {
+                sp_fb.child(user_name.toLowerCase()).set({
+                    email: email,
+                    newsletter: newsletter,
+                    password: password,
+                    poster: poster,
+                    shoveler: shoveler,
+                    user_name: user_name
+                });
+                alert("woot");
+                enable_elements();
             }
         }, 200);
     });
     
     $("#log_in_button").click(function() {
+        disable_elements();
         $(".pop_target").each(function() {
             $(this).popover("destroy");
         });
-        
         var errors = [];
         var log_id = $("#log_id").val();
         var log_password = $("#log_password").val();
+        var remember_me = $("#remember_me_box").is(":checked");
         
         if (log_id === "") {
             pop_set("#log_id", ".popover:contains(name or email)", "Please enter your user name or email.", "right", errors);
@@ -113,6 +152,38 @@ $(document).ready(function() {
                 for (var each in errors) {
                     $(errors[each]).popover("show");
                 }
+                enable_elements();
+            } else {
+                sp_fb.once("value", function(dataSnapshot) {
+                    dataSnapshot.forEach(function(childSnapshot) {
+                        if (log_id !== childSnapshot.child("email").val() && log_id !== childSnapshot.child("user_name").val()) {
+                            pop_set("#log_id", ".popover:contains(ID)", "Invalid log in ID.", "right", errors);
+                        } else {
+                            for (var i = errors.length - 1; i >=0; i--) {
+                                if (errors[i] === "#log_id") {
+                                    errors.splice(i, 1);
+                                }
+                            }
+                            if (log_password !== childSnapshot.child("password").val()) {
+                                pop_set("#log_password", ".popover:contains(Invalid password)", "Invalid password.", "right", errors);
+                                return true;
+                            } else {
+                                return true;
+                            }
+                        }
+                    });
+                    setTimeout(function() {
+                        if (errors.length > 0) {
+                            for (var each in errors) {
+                                $(errors[each]).popover("show");
+                            }
+                            enable_elements();
+                        } else {
+                            alert("Woot");
+                            enable_elements();
+                        }
+                    }, 200);
+                });
             }
         }, 200);
     });
