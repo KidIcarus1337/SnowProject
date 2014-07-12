ec0f313eca25b5b3fd83005b4824709bc84f81c4
KidIcarus1337 <kid_icarus3@yahoo.com>
2014-07-11 23:16:35 -0500

    Another push.

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 var express = require("express");
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('cookie-session');
 
 var app = express();
-app.use(express.bodyParser());
+app.use(bodyParser.json());
+app.use(cookieParser('notsosecretkey'));
+app.use(session({secret: 'notsosecretkey123'}));
 app.use(express.static(__dirname + "/public"));
+
+function getName(req, res) {
+    if (req.session.name) {
+        return res.json({ name: req.session.name });
+    }
+    else {
+        return res.json({ name: '' });
+    }
+}
+
+function setName(req, res) {
+    if(!req.body.hasOwnProperty('name')) {
+        res.statusCode = 400;
+        return res.json({ error: 'Invalid message' });
+    }
+    else {
+        req.session.name = req.body.name;
+        return res.json({ name: req.body.name });
+    }
+}
+
+function logout(req, res) {
+    req.session = null;
+    return res.json({});
+}
 
 function home(req, res) {
     res.sendfile("public/index.html");
